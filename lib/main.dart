0e4bec2c3af01e5a15a170801c7bf4a66bfa7485
Filip <f.cieminski@gmail.com>
2019-12-06 17:46:56 +0100

    first ever statefull widget!

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,55 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
         appBar: AppBar(
           title: Text('Hello'),
           centerTitle: true,
+          backgroundColor: Colors.deepOrange,
         ),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              'Just trying some crazy stuff with Flutter, click button!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey,
+                letterSpacing: 2,
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatButton(),
+      ),
+    );
+  }
+}
+
+class FloatButton extends StatefulWidget {
+  @override
+  _FloatButtonState createState() => _FloatButtonState();
+}
+
+class _FloatButtonState extends State<FloatButton> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FloatingActionButton(
+        child: Text('+ $count'),
+        backgroundColor: Colors.deepOrange,
+        onPressed: () => setState(() => count++),
+        tooltip: 'Increment Counter',
       ),
     );
   }
