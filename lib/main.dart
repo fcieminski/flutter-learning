a2797787c209d31757b2ff1e7550da543d56bcf5
Filip <f.cieminski@gmail.com>
2019-12-08 19:41:24 +0100

    a bit of styling

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
 
 class Home extends StatelessWidget {
   final List<Transaction> transactions = [
-    Transaction(id: '1', title: 'Pc', amount: 1450.30, time: DateTime.now()),
+    Transaction(id: '1', title: 'Pc', amount: 1450.35, time: DateTime.now()),
     Transaction(id: '2', title: 'Shoes', amount: 150, time: DateTime.now()),
     Transaction(
-        id: '3', title: 'Groceries', amount: 98.40, time: DateTime.now()),
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
   ];
 
   @override
@@ -49,7 +49,60 @@ class Home extends StatelessWidget {
           ),
           Column(
             children: transactions.map((transaction) {
-              return Card(child: Text(transaction.title));
+              return Container(
+                child: Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.orangeAccent,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          transaction.amount.toString(),
+                          style: TextStyle(
+                            color: Colors.orangeAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(bottom: 8),
+                              child: Text(
+                                transaction.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                '${transaction.time.month}.${transaction.time.year} ${transaction.time.hour}:${transaction.time.minute}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             }).toList(),
           )
         ],
