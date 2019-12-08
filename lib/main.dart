859a2e2cf0f3b7ade4de482add880524fb5b7a98
Filip <f.cieminski@gmail.com>
2019-12-08 19:08:10 +0100

    building a transaction list from tutorial (after that I'll add some my own tweaks)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,10 +11,49 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: '1', title: 'Pc', amount: 1450.30, time: DateTime.now()),
+    Transaction(id: '2', title: 'Shoes', amount: 150, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.40, time: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New App'),
+        elevation: 5,
+        backgroundColor: Colors.deepOrange,
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            child: Container(
+              child: Text("hello"),
+              width: double.infinity,
+              height: 100,
+            ),
+            elevation: 5,
+          ),
+          Card(
+            child: Container(
+              child: Text('hello2'),
+              width: double.infinity,
+              height: 100,
+            ),
+            elevation: 5,
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(child: Text(transaction.title));
+            }).toList(),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime time;
+
+  Transaction({this.title, this.amount, this.id, this.time});
+}
