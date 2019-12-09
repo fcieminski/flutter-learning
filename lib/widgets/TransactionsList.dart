1099b46d630f3aba3ac3ce8a5a2e4c7e34d6b191
Filip <f.cieminski@gmail.com>
2019-12-09 22:05:03 +0100

    scrolling widget

--- lib/widgets/TransactionsList.dart
+++ lib/widgets/TransactionsList.dart
@@ -9,67 +9,64 @@ class TransactionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((transaction) {
-            return Container(
-              child: Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.all(8),
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.orangeAccent,
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        '${transaction.amount} zł',
-                        style: TextStyle(
-                          color: Colors.orangeAccent,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.orangeAccent,
+                      width: 2,
                     ),
-                    Container(
-                      margin: EdgeInsets.all(8),
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.only(bottom: 8),
-                            child: Text(
-                              transaction.title,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            child: Text(
-                              '${transaction.time.month}.${transaction.time.year} ${transaction.time.hour}:${transaction.time.minute}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.black45,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                  ),
+                  child: Text(
+                    '${transactions[index].amount} zł',
+                    style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            );
-          }).toList(),
-        ),
+                Container(
+                  margin: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(bottom: 8),
+                        child: Text(
+                          transactions[index].title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          '${transactions[index].time.month}.${transactions[index].time.year} ${transactions[index].time.hour}:${transactions[index].time.minute}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black45,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
