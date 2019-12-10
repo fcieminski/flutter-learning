4729d1d6a26bcf0dacd7b2217f813506dcd35050
Filip <f.cieminski@gmail.com>
2019-12-10 22:34:27 +0100

    refactor (i was sure its going to happen)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/models/transaction.dart';
+import 'package:mobile_app/widgets/NewTransaction.dart';
+import 'package:mobile_app/widgets/TransactionsList.dart';
 import './widgets/UserTransaction.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +13,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<Transaction> _transactions = [
+    Transaction(id: '1', title: 'Pc', amount: 1450.35, time: DateTime.now()),
+    Transaction(id: '2', title: 'Shoes', amount: 150, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+    Transaction(
+        id: '3', title: 'Groceries', amount: 98.45, time: DateTime.now()),
+  ];
+
+  void _addTransaction(String title, double amount) {
+    setState(() {
+      _transactions.add(new Transaction(
+        id: DateTime.now().toString(),
+        time: DateTime.now(),
+        title: title,
+        amount: amount,
+      ));
+    });
+  }
+
+  void modalNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context, builder: (_) => NewTransaction(_addTransaction));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +72,14 @@ class Home extends StatelessWidget {
           Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[UserTransactions()],
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    NewTransaction(_addTransaction),
+                    TransactionsList(_transactions),
+                  ],
+                ),
+              ],
             ),
           ),
           FloatingActionButton(
